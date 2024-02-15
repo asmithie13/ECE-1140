750938ba9d187e085baef70b04ee95b7a6ac196c
Abby <abby.magistro@gmail.com>
2024-02-15 14:30:56 -0500

    Added Functionality to Schedule Upload Button

--- CTC/CTC_UI.py
+++ CTC/CTC_UI.py
@@ -10,9 +10,24 @@ class UI(QtWidgets.QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi("CTC/CTC_UI.ui", self)
-        self.show()
+
+        #Connect Buttons
+        self.UploadButton.clicked.connect(self.open_files)
+
+
+    #Define functionality for Upload File Button
+    def open_files(self):
+        # Open a file dialog to select a Excel File
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Select Schedule File", "", "Excel Workbook (*.xlsx);;All Files (*)")
+
+        if file_path:
+            # Implement your logic with the selected file path
+            print(f"Selected PLC File: {file_path}")
+        
 
 
 test = QtWidgets.QApplication(sys.argv)
 window = UI()
+window.show()
 test.exec_()

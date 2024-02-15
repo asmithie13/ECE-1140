91950058819ece84679f56614997a9b0860be0cb
Alex Smith <ajs392@pitt.edu>
2024-02-14 23:35:36 -0500

    Added a simple Block class and have the UI remember prior selections.

---
+++ Wayside SW/Block.py
@@ -0,0 +1,8 @@
+# Define the Block class
+class Block:
+    def __init__(self, light, crossing, switch, state):
+        self.LIGHT = light
+        self.CROSSING = crossing
+        self.SWITCH = switch
+        self.state = state
+
--- Wayside SW/app.py
+++ Wayside SW/app.py
@@ -2,22 +2,23 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore, QtWidgets, uic, QtGui
+from Block import Block
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("Wayside SW/Wayside_UI_Rough.ui",self)
 
-        #Define special blocks constants
-        LIGHT = [True,False,False,True]
-        CROSSING = [False,True,False,True]
-        SWITCH = [False,False,True,True]
+        # Global constants for LIGHT, CROSSING, and SWITCH
+        LIGHT_CONST = [True, False, False, True]
+        CROSSING_CONST = [False, True, False, True]
+        SWITCH_CONST = [False, False, True, True]
 
         #Defining important blocks
-        B3 = CROSSING    
-        B5 = SWITCH
-        B6 = LIGHT
-        B11 = LIGHT
+        B3 = Block(*CROSSING_CONST) 
+        B5 = Block(*SWITCH_CONST) 
+        B6 = Block(*LIGHT_CONST)
+        B11 = Block(*LIGHT_CONST)
 
         #Defines an array of these blocks
 
@@ -28,6 +29,10 @@ class MyApp(QMainWindow):
         self.modeButton.clicked.connect(self.changeMode)
         self.selectLine.stateChanged.connect(self.checkLine)
         self.blockMenu.currentIndexChanged.connect(self.blockActions)
+        self.greenButton.clicked.connect(self.greenButtonPushed)
+        self.redButton.clicked.connect(self.redButtonPushed)
+        self.upCrossingButton.clicked.connect(self.upButtonPushed)
+        self.downCrossingButton.clicked.connect(self.downButtonPushed)
 
         #Original Map Image
         pixmap = QPixmap('Blue Line Images\BlueLine.png')
@@ -80,24 +85,49 @@ class MyApp(QMainWindow):
         selectedIndex = self.blockMenu.currentIndex()
         selectedBlock = self.BlockArray[selectedIndex]
 
-        if selectedBlock[0]:
-            self.greenButton.setEnabled(True)
-            self.redButton.setEnabled(True)
+        if selectedBlock.LIGHT:
+            self.greenButton.setEnabled(not selectedBlock.state)
+            self.redButton.setEnabled(selectedBlock.state)
             self.upCrossingButton.setEnabled(False)
             self.downCrossingButton.setEnabled(False)
             self.switchButton.setEnabled(False)
-        elif selectedBlock[1]:
+        elif selectedBlock.CROSSING:
             self.greenButton.setEnabled(False)
             self.redButton.setEnabled(False)
-            self.upCrossingButton.setEnabled(True)
-            self.downCrossingButton.setEnabled(True)
+            self.upCrossingButton.setEnabled(not selectedBlock.state)
+            self.downCrossingButton.setEnabled(selectedBlock.state)
             self.switchButton.setEnabled(False)
-        elif selectedBlock[2]:
+        elif selectedBlock.SWITCH:
             self.greenButton.setEnabled(False)
             self.redButton.setEnabled(False)
             self.upCrossingButton.setEnabled(False)
             self.downCrossingButton.setEnabled(False)
             self.switchButton.setEnabled(True)
+
+    def greenButtonPushed(self):
+        selectedIndex = self.blockMenu.currentIndex()
+        self.BlockArray[selectedIndex].state = True
+        self.greenButton.setEnabled(False)
+        self.redButton.setEnabled(True)
+
+    def redButtonPushed(self):
+        selectedIndex = self.blockMenu.currentIndex()
+        self.BlockArray[selectedIndex].state = False
+        self.greenButton.setEnabled(True)
+        self.redButton.setEnabled(False)
+
+    def upButtonPushed(self):
+        selectedIndex = self.blockMenu.currentIndex()
+        self.BlockArray[selectedIndex].state = True
+        self.upCrossingButton.setEnabled(False)
+        self.downCrossingButton.setEnabled(True)
+
+    def downButtonPushed(self):
+        selectedIndex = self.blockMenu.currentIndex()
+        self.BlockArray[selectedIndex].state = False
+        self.upCrossingButton.setEnabled(True)
+        self.downCrossingButton.setEnabled(False)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

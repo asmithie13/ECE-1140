232275909b087c006730c5157dcd30ec87adbe83
Alex Smith <ajs392@pitt.edu>
2024-02-15 16:04:07 -0500

    Created initial testbench UI.

--- Wayside SW/Block.py
+++ Wayside SW/Block.py
@@ -1,9 +1,10 @@
 # Define the Block class
 class Block:
-    def __init__(self, light, crossing, switch, state,occupied):
+    def __init__(self, light, crossing, switch, state,occupied,id):
         self.LIGHT = light
         self.CROSSING = crossing
         self.SWITCH = switch
         self.state = state
         self.occupied = occupied
+        self.ID = id
 

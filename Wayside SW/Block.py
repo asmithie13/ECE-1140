10fa8c6307cd006177fa6e622fb82ffc70ce1c26
Alex Smith <ajs392@pitt.edu>
2024-02-15 12:31:29 -0500

    Added more information to block class and have started defining occupancies.

--- Wayside SW/Block.py
+++ Wayside SW/Block.py
@@ -1,8 +1,9 @@
 # Define the Block class
 class Block:
-    def __init__(self, light, crossing, switch, state):
+    def __init__(self, light, crossing, switch, state,occupied):
         self.LIGHT = light
         self.CROSSING = crossing
         self.SWITCH = switch
         self.state = state
+        self.occupied = occupied
 

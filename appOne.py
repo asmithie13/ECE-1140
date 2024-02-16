685c8405da21ec736ceec7c39e0d9fab14754b6e
20lillianjones <lej40@pitt.edu>
2024-02-16 17:13:00 -0500

    added additional functionality to UI

--- appOne.py
+++ appOne.py
@@ -18,6 +18,17 @@ class TrackController_UI(QMainWindow):
         super(TrackController_UI, self).__init__()
         uic.loadUi("TrackControllerHW_UI.ui", self)
 
+        #Disable red and green line selections - Not yet implemented:
+        self.radioButtonRed.setEnabled(False) #TO BE REMOVED LATER
+        self.radioButtonGreen.setEnabled(False)
+
+        #Disable automatic mode button until a PLC file is uploaded:
+        self.checkAuto.setEnabled(False)
+
+        #Set wayside and block selections are disabled before a line is selected:
+        self.comboBoxWayside.setEnabled(False)
+        self.comboBoxBlock.setEnabled(False)
+
         #Signals for PLC upload menu:
         self.buttonBrowse.clicked.connect(self.uploadPLCFile)
         #self.buttonSave.clicked.connect(self.savePLCFile)
@@ -38,26 +49,37 @@ class TrackController_UI(QMainWindow):
 
         if(PLCvalid(fileName) == True):
             self.lineEditBrowse.setText(fileName)
-            self.labelUploadValid.setText("File Uploaded Successfully")
+            #self.labelUploadValid.setText("File Uploaded Successfully")
+            self.buttonBrowse.setEnabled(False) #Cannot upload another PLC file if one is already uploaded
+            #Only option is to switch to manual operation from this point
+
+            self.checkAuto.setEnabled(True)
             self.checkAuto.setChecked(True)
             self.automaticMode()
         else:
             self.lineEditBrowse.setText("")
-            self.labelUploadValid.setText("Invalid File")
+            #self.labelUploadValid.setText("Invalid File")
+        
 
     #def savePLCFile(self):
         #Implement later (If implementing the "Download PLC" feature)
     
     #Occurs if the blue line is selected:
     def blueLineButton(self):
+        self.comboBoxWayside.setEnabled(True)
+        self.comboBoxBlock.setEnabled(True)
         print("Selected: Blue Line") #Remove later after adding actual functionality
 
     #Occurs if the red line is selected:
     def redLineButton(self):
+        self.comboBoxWayside.setEnabled(True)
+        self.comboBoxBlock.setEnabled(True)
         print("Selected: Red Line")
 
     #Occurs if the green line is selected:
     def greenLineButton(self):
+        self.comboBoxWayside.setEnabled(True)
+        self.comboBoxBlock.setEnabled(True)
         print("Selected: Green Line")
 
     #Occurs if the system is in automatic operation:
@@ -79,17 +101,20 @@ class TrackController_TestBench(QMainWindow):
         super(TrackController_TestBench, self).__init__()
         uic.loadUi("TrackControllerHW_TestBench.ui", self)
 
-#Create instance of window:
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    windowOne = TrackController_UI()
-    windowOne.show()
+#Intialize Wayside object
+class Wayside():
+    def __init__(self):
+        #Create instance of window:
+        if __name__ == "__main__":
+            app = QApplication(sys.argv)
+            windowOne = TrackController_UI()
+            windowOne.show()
 
-    windowTwo = TrackController_TestBench()
-    windowTwo.show()
+            windowTwo = TrackController_TestBench()
+            windowTwo.show()
 
-    sys.exit(app.exec_())
+            sys.exit(app.exec_())
 
-
+waysideOne = Wayside()
 
 

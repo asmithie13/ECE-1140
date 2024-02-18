31ada2698942e6cd27356429c377d74ed25d4b48
21AQN01 <122401921+21AQN01@users.noreply.github.com>
2024-02-17 21:05:01 -0500

    test inputs time

--- Track Model/app.py
+++ Track Model/app.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QComboBox, QHBoxLayout, QWidget, QPushButton, QSizePolicy
 from PyQt5 import uic
-from PyQt5.QtCore import Qt  # Import Qt
+from PyQt5.QtCore import Qt  
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
@@ -25,7 +25,7 @@ class MyMainWindow(QMainWindow):
 
         # Dropdown menu for line selection
         line_selection = QComboBox()
-        line_selection.addItems(['Blue', 'Green', 'Red']) 
+        line_selection.addItems(['Blue']) 
         line_selection.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)  # Set size policy to fixed
         line_selection.setFixedWidth(189)  # Set fixed width
         layout.addWidget(line_selection)
@@ -35,6 +35,48 @@ class MyMainWindow(QMainWindow):
         
         # Set the layout for the central widget
         central_widget.setLayout(layout)
+
+        # Connect button to method
+        self.offButton_1.clicked.connect(self.toggle_button_state)
+        self.offButton_2.clicked.connect(self.toggle_button_state_2)
+        self.offButton_3.clicked.connect(self.toggle_button_state_3)
+
+        # Set default state for toggle button
+        self.offButton_1.setText("OFF")
+        self.offButton_1.setStyleSheet("background-color: red;")
+
+        self.offButton_2.setText("OFF")
+        self.offButton_2.setStyleSheet("background-color: red;")
+
+        self.offButton_3.setText("OFF")
+        self.offButton_3.setStyleSheet("background-color: red;")
+
+    def toggle_button_state(self):
+        # Toggle button state and color button 1
+        if self.offButton_1.text() == "OFF":
+            self.offButton_1.setText("ON")
+            self.offButton_1.setStyleSheet("background-color: green;")
+        else:
+            self.offButton_1.setText("OFF")
+            self.offButton_1.setStyleSheet("background-color: red;")
+
+    def toggle_button_state_2(self):
+        # Toggle button state and color button 2
+        if self.offButton_2.text() == "OFF":
+            self.offButton_2.setText("ON")
+            self.offButton_2.setStyleSheet("background-color: green;")
+        else:
+            self.offButton_2.setText("OFF")
+            self.offButton_2.setStyleSheet("background-color: red;")
+
+    def toggle_button_state_3(self):
+        # Toggle button state and color button 3
+        if self.offButton_3.text() == "OFF":
+            self.offButton_3.setText("ON")
+            self.offButton_3.setStyleSheet("background-color: green;")
+        else:
+            self.offButton_3.setText("OFF")
+            self.offButton_3.setStyleSheet("background-color: red;")
         
     def upload_track_layout(self):
         file_dialog = QFileDialog()
@@ -42,33 +84,40 @@ class MyMainWindow(QMainWindow):
         if file_path:
             self.label.setText("File uploaded successfully")
 
-    class Data:
-        def __init__(self):
-            #Initializing variables
-            self.elevation = None
-            self.grade = None
-            self.length1 = None
-            self.temp = None
-            self.heaters = None
-            self.occupancy = None
-            self.broken_rail = None
-            self.circuit_failure = None
-            self.power_failure = None
-            self.block_num = None
-            self.direction = None
-            self.cross = None
 
+class TestBench(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Track Model/testbench_trackmodel.ui", self)
 
+class Data:
+    def __init__(self):
+        #Initializing variables
+        self.elevation = None
+        self.grade = None
+        self.length1 = None
+        self.temp = None
+        self.heaters = None
+        self.occupancy = None
+        self.broken_rail = None
+        self.circuit_failure = None
+        self.power_failure = None
+        self.block_num = None
+        self.direction = None
+        self.cross = None
+
+    def get_elevation(self):
+        #start it bro
+        pass
         
-        def get_elevation(self):
-            #start it bro
-            pass
-        
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
+    window_2 = TestBench()
+
     window.show()
+    window_2.show()
+
     sys.exit(app.exec_())
 

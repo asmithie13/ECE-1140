462e1e04fc7fd19f42af1c2e793825af6abc4149
20lillianjones <lej40@pitt.edu>
2024-02-19 13:00:04 -0500

    almost finalized block class

--- Block.py
+++ Block.py
@@ -3,47 +3,26 @@ import csv
 #Implementation of a block class:
 class Block:
     def __init__(self, lineColor, blockSection, blockNum, hasSwitch, hasCrossing):
+        #Assign arguments:
         self.lineColor = lineColor
         self.blockSection = blockSection
         self.blockNum = blockNum
         self.hasSwitch = hasSwitch
         self.hasCrossing = hasCrossing
 
+        #All blocks are initialized as unoccupied and operational:
         self.isOccupied = False
         self.isFailed = False
+
+        #All block ttributes begin in the default position:
         self.switchState = False
         self.lightState = False
         self.crossingState = False
-    
-    #Function to return the line that the block is on:
-    def returnLineColor(self):
-       return self.lineColor
     
     #Function to return the block ID of the block:
     def returnBlockID(self):
        tempStr = self.blockSection + str(self.blockNum)
        return tempStr
-
-    #Function to set the occupancy of a block:
-    def setOccupied(self, occState):
-       self.isOccupied = occState
-    
-    #Function to return the occupancy state of a block
-    def getOccupancy(self):
-       return self.isOccupied
-    
-    #Function to set the failure status of a block:
-    def setFailed(self, failState):
-       self.isFailed = failState
-    
-    #Function to return the failure status of a block
-    def getFailed(self):
-       return self.isFailed
-    
-    #Function to set the state of a switch:
-    def changeSwitch(self, switchState):
-       if(self.hasSwitch == True):
-         self.switchState = switchState
       
     #Function to get the state of a switch:
     def getSwitchState(self):
@@ -55,10 +34,6 @@ class Block:
           else:
              return "Right"
        return self.switchState
-    
-    #Function to set the state of a light:
-    def changeLight(self, lightState):
-       self.lightState = lightState
       
     #Function to get the state of a light:
     def getLightState(self):
@@ -69,11 +44,6 @@ class Block:
              return "Red"
           else:
              return "Green"
-    
-    #Function to set the state of a crossing:
-    def changeCrossing(self, crossingState):
-       if(self.hasCrossing == True):
-         self.crossingState = crossingState
       
     #Function to get the state of a crossing:
     def getCrossingState(self):

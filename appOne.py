562af25692bb748465bd24cf0d2fc682b0944d4c
20lillianjones <lej40@pitt.edu>
2024-02-19 03:01:03 -0500

    added more functionality to testbench

--- appOne.py
+++ appOne.py
@@ -5,8 +5,8 @@ import sys
 from Block import Block
 import csv
 
-#Function to confirm whether or not the selected PLC file valid
-def PLCvalid(fileName):
+#Function to confirm whether or not the selected PLC file valid:
+def PLCvalid(fileName): #Reads based-on heading - Different text can be specified
         fileOpen = open(fileName, 'r')
         headLine = fileOpen.readline()
         if(headLine != "PLC File"):
@@ -31,26 +31,29 @@ def readTrackFile(fileName):
             tempBlock = Block(line[0], line[1], line[2], hasSwitchTemp, hasCrossingTemp)
             totalBlocks.append(tempBlock)
     
-    return totalBlocks
+    return totalBlocks #Return a list of all blocks within the file
 
-#Initialization of UI
+#Initialization of UI:
 class TrackController_UI(QMainWindow):
     def __init__(self):
         #Load-in UI from the TrackControllerHW_UI file:
         super(TrackController_UI, self).__init__()
         uic.loadUi("TrackControllerHW_UI.ui", self)
 
-        #Read all blocks and their attributes:
+        #Block that is being acted upon at any one time
+        self.selectedBlock = Block("Blue", "A", 1, False, False)
+
+        #Read all blocks and their attributes (Crossings, switches, etc.)
         self.allBlueBlocks = readTrackFile("blueLine.csv")
         self.allRedBlocks = readTrackFile("redLine.csv")
         self.allGreenBlocks = readTrackFile("greenLine.csv")
 
         #Disable the "Browse" button until a line is selected:
-        self.buttonBrowse.setEnabled(False)
+        self.buttonBrowse.setEnabled(False) #Leave disabled until automatic mode is fully implemented
 
         #Disable red and green line selections - Not yet implemented:
         #Before implementing - Must determine which wayside stations cover which blocks:
-        self.radioButtonRed.setEnabled(False) #TO BE REMOVED LATER
+        self.radioButtonRed.setEnabled(False)
         self.radioButtonGreen.setEnabled(False)
 
         #Disable automatic mode button until a PLC file is uploaded:
@@ -65,7 +68,6 @@ class TrackController_UI(QMainWindow):
 
         #Signals for PLC upload menu:
         self.buttonBrowse.clicked.connect(self.uploadPLCFile)
-        #self.buttonSave.clicked.connect(self.savePLCFile)
 
         #Signals for line selection:
         self.radioButtonBlue.clicked.connect(self.blueLineButton)
@@ -78,6 +80,9 @@ class TrackController_UI(QMainWindow):
 
         #Wayside selection signal:
         self.comboBoxWayside.activated.connect(self.waysideSelect) #Any selection will trigger this
+
+        #Block selection signal:
+        self.comboBoxBlock.activated.connect(self.blockSelect)
     
     #Function to upload the PLC file and display the file location:
     def uploadPLCFile(self):
@@ -136,24 +141,35 @@ class TrackController_UI(QMainWindow):
             self.buttonBrowse.setEnabled(False) #Unable to move back to automatic operation if currently in manual
             self.checkAuto.setChecked(False)
             self.checkAuto.setEnabled(False) 
+            self.lineEditBrowse.setText("")
 
         self.comboBoxWayside.setEnabled(True)
         self.comboBoxBlock.setEnabled(True)
-
-        self.buttonBrowse.setEnabled(False)
-        self.lineEditBrowse.setText("")
-
         print("Mode: Manual")
     
-    #Need to make function for each wayside station
     def waysideSelect(self):
-        self.buttonBrowse.setEnabled(True)
+        #self.buttonBrowse.setEnabled(True) #Automatic not yet implemented, so function is commented-out
         if(self.comboBoxWayside.currentText() == "Blue"): #If the "Blue" wayside is selected (Temporary wayside for the blue line):
             pixmap = QPixmap("blueline.png")
             self.labelPhoto.setPixmap(pixmap)
             self.comboBoxBlock.setEnabled(True)
             for blockNum in self.allBlueBlocks:
                 self.comboBoxBlock.addItem(blockNum.returnBlockID())
+    
+    #Function to handle selected block:
+    def blockSelect(self):
+        selectedBlockText = self.comboBoxBlock.currentText()
+        for block in self.allBlueBlocks:
+            if(block.returnBlockID() == selectedBlockText):
+                self.selectedBlock = block
+                break
+
+    #NEXT:
+        #Write functions that allow the Arduino to serially change the switch state,
+        #light state, and crossing state of a self.selectedBlock, and then return the edited block
+        #to the main block index (here, self.allBlueBlocks)
+            
+        #SEND FROM TESTBENCH TO UI/UI TO TESTBENCH
 
 #Initialization of test bench:
 class TrackController_TestBench(QMainWindow):
@@ -164,25 +180,59 @@ class TrackController_TestBench(QMainWindow):
         #Receive the text from the type inputs:
         self.lineEditSpeedInput.returnPressed.connect(self.sendSpeed)
         self.lineEditAuthorityInput.returnPressed.connect(self.sendAuthority)
-        self.lineEditOccupiedInput.returnPressed.connect(self.sendOccupied)
-        self.lineEditFailedInput.returnPressed.connect(self.sendFailed)
+
+        #Receive blocks from combo box:
+        self.comboBoxOccIn.activated.connect(self.sendOccupied)
+        self.comboBoxFailedIn.activated.connect(self.sendFailed)
+
+        self.totalBlueBlock = readTrackFile("blueLine.csv")
+        for block in self.totalBlueBlock:
+            self.comboBoxOccIn.addItem(block.returnBlockID())
+            self.comboBoxFailedIn.addItem(block.returnBlockID())
+            self.comboBoxCheckBlock.addItem(block.returnBlockID())
+        
+        #Clear block fields:
+        self.buttonClearOcc.clicked.connect(self.clearOccupied)
+        self.buttonClearFailed.clicked.connect(self.clearFailed)
     
+    #Send input speed to output (Wayside does not change):
     def sendSpeed(self):
         inputSpeed = self.lineEditSpeedInput.text()
-        print(inputSpeed)
+        self.lineEditSpeedOut.setText(inputSpeed)
     
+    #Handle authority input:
     def sendAuthority(self):
         inputAuthority = self.lineEditAuthorityInput.text()
-        
-        print(inputAuthority)
+        #Send the input authority to the UI to be displayed, and receive the
+        #user-entered commanded authority from the UI to display on the testbench
     
+    #Handle occupancy input:
     def sendOccupied(self):
-        inputBlock = self.lineEditOccupiedInput.text()
-        print(inputBlock)
+        selectedBlock = self.comboBoxOccIn.currentText()
+        currentText = self.lineEditOccupiedOut.text()
+        tempStr = currentText + " " + selectedBlock
+        self.lineEditOccupiedOut.setText(tempStr)
+        #Send this string to the UI to be displayed
     
+    #Handle failure input:
     def sendFailed(self):
-        inputBlock = self.lineEditFailedInput.text()
-        print(inputBlock)
+        selectedBlock = self.comboBoxFailedIn.currentText()
+        currentText = self.lineEditFailedOut.text()
+        tempStr = currentText + " " + selectedBlock
+        self.lineEditFailedOut.setText(tempStr)
+        #Send this string to the UI to be displayed
+    
+    #Handle clear buttons:
+    def clearOccupied(self):
+        self.lineEditOccupiedOut.clear()
+    
+    def clearFailed(self):
+        self.lineEditFailedOut.clear()
+
+    #Function to check block states:
+    def checkBlockStates(self):
+        selectedBlock = self.comboBoxCheckBlock.currentText()
+        #Find the block object with the corresponding ID from the UI, and display the states here
 
 #Intialize Wayside object
 class Wayside():

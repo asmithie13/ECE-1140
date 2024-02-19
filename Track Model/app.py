5486ce2995e7e64a6cc60b07ad20d94b9c0cd759
21AQN01 <122401921+21AQN01@users.noreply.github.com>
2024-02-19 15:07:57 -0500

    testbench2

--- Track Model/app.py
+++ Track Model/app.py
@@ -4,16 +4,6 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt  
 
 # Define the Block class
-class Block:
-    def __init__(self, light, crossing, switch, state,occupied,id):
-        self.LIGHT = light
-        self.CROSSING = crossing
-        self.SWITCH = switch
-        self.state = state
-        self.occupied = occupied
-        self.ID = id
-        
-
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()

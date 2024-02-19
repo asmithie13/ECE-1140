39a21e6a29301a6c8a67d8c8c9336c67aa782555
Tanvi <tanvi.verma.214@gmail.com>
2024-02-19 00:06:31 -0500

    02-19 TrainModel progress

--- Train Model/app.py
+++ Train Model/app.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from TrainModel_UI import Ui_MainWindow  # Import the generated UI class
+#from TrainModel_UI import Ui_MainWindow  # Import the generated UI class
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore, QtWidgets, uic, QtGui
 
@@ -8,16 +8,13 @@ class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("Train Model/TrainModel_UI.ui", self)
-
-class MyMainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        #self.ui = Ui_MainWindow()
+        #self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("windows")
     window = MyMainWindow()
     window.show()
     sys.exit(app.exec_())

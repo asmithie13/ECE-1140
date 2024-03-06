5db8e00dae79942d44a916e98a0baca18e554eb5
20lillianjones <lej40@pitt.edu>
2024-03-05 23:07:58 -0500

    added comments

--- Wayside HW/TrackControllerHW.py
+++ Wayside HW/TrackControllerHW.py
@@ -13,8 +13,10 @@ class TrackControllerHW():
         windowOne = TrackController_UI()
         windowTwo = TrackController_TestBench()
 
+        #Signal to send a list of changed block objects to the track model module:
         windowOne.blockStates.connect(windowTwo.getUpdatedBlockList)
 
+        #Signals to send block occupancies from test bench to UI (later to be received from track model)
         windowTwo.occBlocksChanged.connect(windowOne.displayOccupancies)
         windowTwo.failedBlocksChanged.connect(windowOne.displayFailures)
 

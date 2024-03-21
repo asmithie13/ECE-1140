34856a286acc34808fe032404d14fae434c5d807
asmithie13 <ajs392@pitt.edu>
2024-03-21 15:32:23 -0400

    Updated block class to include Wayside.

--- Wayside_SW/WaysideSWandTB.py
+++ Wayside_SW/WaysideSWandTB.py
@@ -93,6 +93,8 @@ class WaysideSW(QMainWindow):
         #Defines special greenblocks in wayside 1     
         for block in self.greenWayside2Blocks:
             if block.LIGHT or block.CROSSING or block.SWITCH : self.specialGreenBlocksW2.append(block)
+            block.Wayside = "W2"
+
 
         #Defines Red line blocks
         self.redCrossingTriplesIDS = [] #ids of red crossing blocks
@@ -106,9 +108,11 @@ class WaysideSW(QMainWindow):
 
         for block in self.redWayside1Blocks:
             if block.LIGHT or block.CROSSING or block.SWITCH : self.specialRedBlocksW1.append(block)
+            block.Wayside = "W1"
 
         for block in self.redWayside2Blocks:
             if block.LIGHT or block.CROSSING or block.SWITCH : self.specialRedBlocksW2.append(block)
+            block.Wayside = "W2"
 
         #Create Parser Object
         self.currentSwitchBlocksNums = [['5','6','11']]

6271ec64454e69c16a042d85ff56e83852dfe4da
asmithie13 <ajs392@pitt.edu>
2024-03-23 18:40:26 -0400

    Green line arrs created to keep track of 5 block occupancy limit.

--- Wayside_SW/WaysideSWandTB.py
+++ Wayside_SW/WaysideSWandTB.py
@@ -99,6 +99,45 @@ class WaysideSW(QMainWindow):
             if block.LIGHT or block.CROSSING or block.SWITCH : self.specialGreenBlocksW2.append(block)
             block.Wayside = "W2"
 
+        #2D array of blocks within 5 blocks of each other
+        self.green5blocks = []
+
+        for block in self.allGreenBlocks:
+            tempList = []
+            for x in range(5,-1,-1): 
+                temp1 = int(block.blockNum) - x - 1
+                if temp1 < 1: continue
+                tempList.append(temp1)
+            for x in range(5): 
+                temp2 = int(block.blockNum) + x + 1
+                if temp2 > len(self.allGreenBlocks): continue
+                tempList.append(temp2)
+            self.green5blocks.append(tempList)
+
+        switch1 = [5,4,3,2,1,12,13,14,15,16]
+        switch2 = [24,25,26,27,28,150,149,148,147,146]
+        switch3 = [72,73,74,75,76,101,102,103,104,105]
+        switch4 = [81,82,83,84,85,100,101,102,103,104]
+
+        # Define the pattern
+        pattern = [slice(i, i + 5) for i in range(1,6)]
+
+        # Apply the pattern to each switch
+        new_switch1 = [switch1[s] for s in pattern]
+        new_switch2 = [switch2[s] for s in pattern]
+        new_switch3 = [switch3[s] for s in pattern]
+        new_switch4 = [switch4[s] for s in pattern]
+
+        # Apply the pattern to each switch
+        switches = [switch1, switch2, switch3, switch4]
+
+        for switch, pattern in zip(switches, [new_switch1, new_switch2, new_switch3, new_switch4]):
+            for index, arr in zip(switch, pattern):
+                for x in arr:
+                    if x not in self.green5blocks[index - 1]:
+                        self.green5blocks[index - 1].append(x)
+
+       
 
         #Defines Red line blocks
         self.redCrossingTriplesIDS = [] #ids of red crossing blocks
@@ -441,7 +480,9 @@ class WaysideSW(QMainWindow):
                 if block_id == block.ID:
                     block.occupied = True
                     self.occupiedBlocks.append(block)
+            
 
+        
         self.BlockOcc.setText(" ".join(sentBlocks))
         if self.label_7.text() == "AUTOMATIC" : self.FileParser.parsePLC()
         self.blockActions()
@@ -449,8 +490,7 @@ class WaysideSW(QMainWindow):
         self.sendOccupiedBlocks.emit(self.occupiedBlocks)
 
     def receiveSpeedAuth(self,initialTrainSpeedAuthority):
-         self.initialTrainSpeedAuthority.append(initialTrainSpeedAuthority)
-         self.sendTrainSpeedAuth.emit(initialTrainSpeedAuthority)
+        self.sendTrainSpeedAuth.emit(initialTrainSpeedAuthority)
         
 class TestBench(QMainWindow):
 

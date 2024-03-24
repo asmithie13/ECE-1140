f512943d04a776e253cab4233ca448ab53e4c2e8
Lauren Gilfillan <lag179@pitt.edu>
2024-03-24 11:40:04 -0500

    test bench working....mostly

--- Train Controller SW/Authority.py
+++ Train Controller SW/Authority.py
@@ -3,12 +3,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtWidgets
-from PyQt5 import Timer
+#from PyQt5 import Timer
 
 class Vital_Authority():
 
-    def __init__(self,ui):
+    def __init__(self,ui,curr_auth_signal):
         self.ui = ui
+        self.curr_auth_signal = curr_auth_signal
         # self.authTimer = QTimer()
         # self.authTimer.setInterval(1000)
         # self.authTimer.timeout.connect(self.Authority_Monitor)

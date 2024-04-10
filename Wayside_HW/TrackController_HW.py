5ba89083674935b51d3f9eabd6bbe4c0eabdd0c9
20lillianjones <lej40@pitt.edu>
2024-04-10 01:36:55 -0400

    added collision prevention if trailing train is gaining too quickly

--- Wayside_HW/TrackController_HW.py
+++ Wayside_HW/TrackController_HW.py
@@ -13,7 +13,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
 #Enable serial communication:
-serialObject = serial.Serial('COM8', 57600)
+#serialObject = serial.Serial('COM8', 9600)
 
 from Wayside_HW.TrackController_HW_TB import *
 from Wayside_HW.readTrackFile import *
@@ -83,6 +83,7 @@ class TrackController_HW(QMainWindow):
         for block in self.occupiedBlocks: #Set occupancy status in the list of all blocks
             block.occupied = 1
         
+        self.preventCollision()
         self.sendOccupiedBlocks.emit(self.occupiedBlocks)
         listBlockIDOccupied = []
         listBlockStrOccupied = ""
@@ -140,12 +141,10 @@ class TrackController_HW(QMainWindow):
         occupiedBlockSections.sort()
 
         if occupiedBlockSections == self.occupiedBlockSections: #Only proceed if there is a section occupancy change
-            print("NO NEW SECTION OCCUPANCY DETECTED")
             return
         else:
             self.occupiedBlockSections = occupiedBlockSections
-        print("NEW SECTION OCCUPANCY DETECTED")
-        
+
         #Send string with flag at end to send block occupancies serially:
         occupiedBlockString = ""
         for section in occupiedBlockSections:
@@ -154,7 +153,7 @@ class TrackController_HW(QMainWindow):
         occupiedBlockBytes = occupiedBlockString.encode()
 
         '''BEGIN SERIAL COMMUNICATION'''
-        serialObject.write(occupiedBlockBytes)
+        '''serialObject.write(occupiedBlockBytes)
         #Receiving serial responses from the Raspberry Pi:
         copyBlocks = self.allBlocks
         attributeList = []
@@ -168,7 +167,7 @@ class TrackController_HW(QMainWindow):
             if letter == 'A':
                 break
             else:
-                attributeList.append(letter)
+                attributeList.append(letter)'''
         
         #Parse PLC file and adjust blocks accordingly:
         self.allBlocks = newParse(occupiedBlockSections, self.allBlocks)
@@ -181,7 +180,7 @@ class TrackController_HW(QMainWindow):
             elif block.CROSSING == True:
                 attributeListSoftware.append(str(block.crossingState))
 
-        if attributeList != attributeListSoftware:
+        '''if attributeList != attributeListSoftware:
             self.lineEditHardware.setText("ERRORS DETECTED. STOPPING ALL TRAINS.")
             for block in self.allBlocks:
                 block.authority = False
@@ -189,11 +188,10 @@ class TrackController_HW(QMainWindow):
         else:
             #Ajust block-wise authority based on active red lights:
             self.updateBooleanAuth()
-            self.sendUpdatedBlocks.emit(self.allBlocks)
+            self.sendUpdatedBlocks.emit(self.allBlocks)'''
         
-        #self.updateBooleanAuth() #Uncomment when hardware is not connected
-        #self.preventCollision()
-        #self.sendUpdatedBlocks.emit(self.allBlocks) #Uncomment when hardware is not connected
+        self.updateBooleanAuth() #Uncomment when hardware is not connected
+        self.sendUpdatedBlocks.emit(self.allBlocks) #Uncomment when hardware is not connected
     
     def selectBlock(self):
         self.frameLight.setEnabled(False)
@@ -409,21 +407,25 @@ class TrackController_HW(QMainWindow):
                 if block.ID in self.LIGHT_Z150:
                     block.authority = True
     
-    '''def preventCollision(self):
-        oneDirectionOne = ['A', 'B', 'C'] #Blocks where a train coming from behind is at an index GREATER than the train-in front in self.allBlocks
-        oneDirectionTwo = ['G', 'H', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] #Blocks where a train coming from behind is at an index LOWER than the train-in front in self.allBlocks
-        twoDirection = ['D', 'E', 'F',] #Bi-directional track
-
+    def preventCollision(self):
+        oneDirection = ['G', 'H', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] #Block sections where collisions could occur
         tempSkip = []
         for index, block in enumerate(self.allBlocks):
-            if block.blockSection in oneDirectionOne:
-                if block.ID in self.listOccIDs:
-                    self.allBlocks[index+1].authority = False
+            if block.blockSection in oneDirection:
+                if block.blockSection == 'S': #Cannot be iterated through due to being beginning of Wayside #1
+                    continue
+            
+                #If a train is about to collide with the train in front of it, set Boolean authority to zero:
+                if block.ID in self.listOccIDs and self.allBlocks[index-2].ID in self.listOccIDs:
+                    self.allBlocks[index-1].authority = False
                     tempSkip.append(self.allBlocks[index+1].ID)
-                    self.allBlocks[index+2].authority = False
+                    self.allBlocks[index-2].authority = False
                     tempSkip.append(self.allBlocks[index+2].ID)
-                    self.allBlocks[index+3].authority = False
-                    tempSkip.append(self.allBlocks[index+3].ID)
-                else:
-                    if block.ID not in tempSkip and block.ID not in self.LIGHT_A1 and block.ID not in self.LIGHT_C12 and block.ID not in self.LIGHT_G29 and block.ID not in self.LIGHT_Z150:
-                        block.authority = True'''
+                    continue
+                
+                #Otherwise, reset the Boolean authority to 1:
+                if block.ID not in tempSkip:
+                    block.authority = True
+
+
+                

a6b6616639243927f6c48055a553c7176dd20e73
Lauren Gilfillan <lag179@pitt.edu>
2024-04-15 12:35:10 -0400

    fixing the list

--- Train_Controller_SW/NonVital.py
+++ Train_Controller_SW/NonVital.py
@@ -1,4 +1,6 @@
 from Train_Controller_SW.mainControl import Ui_MainWindow
+from Train_Controller_SW import Line_Dictionary
+
 class NonVital():
     def __init__(self,ui, door_control_sig,announcement_sig,
     temp_control_sig,int_light_sig,ext_light_sig,internal_speed_lim_sig):
@@ -13,11 +15,8 @@ class NonVital():
         self.ext_light_sig=ext_light_sig
         self.speed_lim = internal_speed_lim_sig
         #inherited signalsd
-
-        self.blocksTraveledCounter = 0
-        self.blocksToUnderground = 0
-        self.undergroundBlocks = 0
-        self.dist_next_station= 0
+        self.doors = 0
+        self.LineDictionary = Line_Dictionary()
     
     def Control_Headlights_On(self):
             self.ui.buttonHDoff.toggle()
@@ -49,62 +48,62 @@ class NonVital():
             print("interior lights dimmed")
 
 
-    #might not need this function, may be able to connect directly
-   # def Control_Temperature(self):
+#     #might not need this function, may be able to connect directly
+#    # def Control_Temperature(self):
     
-    def LeftStation(self):
-        #open Left doors
-        #send announcement
-        #turn on int lights
-        #wait 60 seconds
-        #close left doors
-        #turn off int lights
-        #send announcement to next station
+#     def LeftStation(self):
+#         #open Left doors
+#         #send announcement
+#         #turn on int lights
+#         #wait 60 seconds
+#         #close left doors
+#         #turn off int lights
+#         #send announcement to next station
 
-        self.ui.buttonDoorL.toggle()
-        self.door_control_sig.emit(2)
-        self.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
+#         self.ui.buttonDoorL.toggle()
+#         self.door_control_sig.emit(2)
+#         self.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
 
-        self.ui.IntLightSld.setValue(1)
-        self.stationTimer.start()
-        self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorL.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
+#         self.ui.IntLightSld.setValue(1)
+#         self.stationTimer.start()
+#         self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorL.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
 
-    def RightStation(self):
-        #open Right doors
-        #send announcement
-        #turn on int lights
-        #wait 60 seconds
-        #close right doors
-        #turn off int lights
-        #send announcement to next station
+#     def RightStation(self):
+#         #open Right doors
+#         #send announcement
+#         #turn on int lights
+#         #wait 60 seconds
+#         #close right doors
+#         #turn off int lights
+#         #send announcement to next station
 
-        self.ui.buttonDoorR.toggle()
-        self.door_control_sig.emit(1)
-        self.ui.SpkrOut.setText("Welcome to " + self.ui.lineEditAnn.text())
-        self.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
-        self.ui.IntLightSld.setValue(1)
-        self.int_light_sig.emit(1)
-        self.stationTimer.start()
+#         self.ui.buttonDoorR.toggle()
+#         self.door_control_sig.emit(1)
+#         self.ui.SpkrOut.setText("Welcome to " + self.ui.lineEditAnn.text())
+#         self.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
+#         self.ui.IntLightSld.setValue(1)
+#         self.int_light_sig.emit(1)
+#         self.stationTimer.start()
 
-        #need to fix this  --- needs to include emit signals
-        self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorR.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
+#         #need to fix this  --- needs to include emit signals
+#         self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorR.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
 
-    def BothStation(self):
-        #open both doors
-        #send announcement
-        #turn on int lights
-        #wait 60 seconds
-        #close both doors
-        #turn off int lights
-        #send announcement to next station
+#     def BothStation(self):
+#         #open both doors
+#         #send announcement
+#         #turn on int lights
+#         #wait 60 seconds
+#         #close both doors
+#         #turn off int lights
+#         #send announcement to next station
 
-        self.ui.buttonDoorL.toggle()
-        self.ui.buttonDoorR.toggle()
-        self.door_control_sig.emit(2)
-        self.ui.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
-        self.ui.IntLightSld.setValue(1)
-        self.stationTimer.start()
-        self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorL.toggle(), self.ui.buttonDoorR.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
+#         self.ui.buttonDoorL.toggle()
+#         self.ui.buttonDoorR.toggle()
+#         self.door_control_sig.emit(2)
+#         self.ui.announcement_sig.emit("Welcome to " + self.ui.lineEditAnn.text())
+#         self.ui.IntLightSld.setValue(1)
+#         self.stationTimer.start()
+#         self.stationTimer.timeout.connect(lambda: self.ui.buttonDoorL.toggle(), self.ui.buttonDoorR.toggle(), self.ui.IntLightSld.setValue(0), self.ui.announcement_sig.emit("Next stop is " + self.ui.lineEditAnn.text()))
 
     def Read_Beacon(self,beacon):
         self.line = beacon     
@@ -117,19 +116,19 @@ class NonVital():
         else:
             self.ui.undergrnd_ind.setStyleSheet("color: rgb(225, 225, 225);\n"
                                         "background-color: rgb(255, 255, 255);")
-            self.int_light_sig.emit(0)
+            self.ext_light_sig.emit(0)
                 
 
 
     def Door(self):
         if(self.ui.buttonDoorR.isChecked() and self.ui.buttonDoorL.isChecked()):
-            self.door_control_sig.emit(3) #check these
+            self.door_control_sig.emit(3) #ALL
         elif(self.ui.buttonDoorR.isChecked() and not(self.ui.buttonDoorL.isChecked())):
-            self.door_control_sig.emit(1) #check these
+            self.door_control_sig.emit(1) # RIGHT
         elif(not(self.ui.buttonDoorR.isChecked()) and self.ui.buttonDoorL.isChecked()):
-            self.door_control_sig.emit(2)  #check these
+            self.door_control_sig.emit(2)  #LEFT
         else:
-            self.door_control_sig.emit(0)
+            self.door_control_sig.emit(0) #NONE
 
 
 
@@ -139,7 +138,18 @@ class NonVital():
         #green line parse
         if self.line == "green" :
             self.speed_lim.emit(self.LineDictionary.green_get_speed_lim(self.block_index))
-            self.Set_Underground(self.self.Line_Dictionary.green_get_underground(self.block_index))
+            self.Set_Underground(self.Line_Dictionary.green_get_underground(self.block_index))
+            self.announcement = self.LineDictionary.get_green_station(self.block_index)
+            if (self.announcement != 'N/A'):
+                if self.announcement[0:6] == "Welcome" :
+                    self.announcement_sig.emit(self.announcement)
+                    self.doors = int(self.LineDictionary.get_green_door_side(self.block_index))
+            elif self.announcement [0:10] == "Approaching" :
+                    self.announcement_sig.emit(self.announcement)
+
+        
+                
+
             
         #red line parse
         elif self.line == "red" :

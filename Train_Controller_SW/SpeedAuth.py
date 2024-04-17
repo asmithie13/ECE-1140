720d45d6a756a63ab4913db5085743c7ec574cd5
Chad <chad.collina@gmail.com>
2024-04-17 09:13:02 -0400

    train runs again

--- Train_Controller_SW/SpeedAuth.py
+++ Train_Controller_SW/SpeedAuth.py
@@ -17,7 +17,7 @@ class Vital_Speed_Auth():
         self.time = 0
         self.prev_time = 0
         self.ebrake_sig = ebrake_internal_sig
-        self.stop_at_staion_sig(bool)
+        self.stop_at_staion_sig = pyqtSignal(bool)
         self.decimal_m_auth = 0
 
     def Control_Current_Speed(self,newSpeed):

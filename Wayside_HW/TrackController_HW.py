9ce6c8d164dc37bfee334d0c0d463b7ad5a19d8c
20lillianjones <lej40@pitt.edu>
2024-04-17 23:20:35 -0400

    fixed small automatic mode collision prevention bugs

--- Wayside_HW/TrackController_HW.py
+++ Wayside_HW/TrackController_HW.py
@@ -13,7 +13,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
 #Enable serial communication:
-#serialObject = serial.Serial('COM8', 9600)
+serialObject = serial.Serial('COM8', 9600)
 
 from Wayside_HW.TrackController_HW_TB import *
 from Wayside_HW.readTrackFile import *
@@ -62,6 +62,8 @@ class TrackController_HW(QMainWindow):
 
         self.maintenanceSwitches = []
 
+        self.closedFlag = 0
+
         #Signals (Manual mode-related):
         self.checkBoxManual.clicked.connect(self.manualMode)
         self.comboBoxSection.activated.connect(self.selectBlock)
@@ -74,6 +76,10 @@ class TrackController_HW(QMainWindow):
         self.pushButtonDown.clicked.connect(self.setCrossingDown)
     
     def modeHandler(self, occupiedBlocks):
+        occupiedBlocks.sort()
+        if occupiedBlocks == self.listOccIDs and self.closedFlag == 0:
+            return
+
         self.previousOccupiedBlock = self.occupiedBlocks
         
         self.listOccIDs = occupiedBlocks #Argument received is a string of occupied block IDs
@@ -108,10 +114,12 @@ class TrackController_HW(QMainWindow):
         self.setMaintenanceSwitch() #Sets any time that there is a new occupancy
         #Must be called again after automatic mode switch positions are determined
 
+        self.closedFlag = 0
         if self.modeFlag == 0:
             self.automaticMode()
         
     def getClosedBlocks(self, closedBlocks):
+        self.closedFlag = 1
         for block in closedBlocks:
             if block.maintenance == 1 and block not in self.closedBlocks:
                 self.closedBlocks.append(block)
@@ -166,7 +174,7 @@ class TrackController_HW(QMainWindow):
         occupiedBlockBytes = occupiedBlockString.encode()
 
         '''BEGIN SERIAL COMMUNICATION'''
-        '''serialObject.write(occupiedBlockBytes)
+        serialObject.write(occupiedBlockBytes)
         #Receiving serial responses from the Raspberry Pi:
         copyBlocks = self.allBlocks
         attributeList = []
@@ -188,7 +196,7 @@ class TrackController_HW(QMainWindow):
                     else:
                         attributeList.append(myAttribute)
                 if breakFlag == 1:
-                    break'''
+                    break
         
         #Parse PLC file and adjust blocks accordingly:
         self.allBlocks = newParse(occupiedBlockSections, self.allBlocks)
@@ -201,7 +209,7 @@ class TrackController_HW(QMainWindow):
             elif block.CROSSING == True:
                 attributeListSoftware.append(str(block.crossingState))
 
-        '''if attributeList != attributeListSoftware:
+        if attributeList != attributeListSoftware:
             self.lineEditHardware.setText("ERRORS DETECTED. STOPPING ALL TRAINS.")
             for block in self.allBlocks:
                 block.authority = False
@@ -211,12 +219,12 @@ class TrackController_HW(QMainWindow):
             self.setMaintenanceSwitch()
             self.updateBooleanAuth()
             self.preventCollision()
-            self.sendUpdatedBlocks.emit(self.allBlocks)'''
+            self.sendUpdatedBlocks.emit(self.allBlocks)
         
-        self.setMaintenanceSwitch()
+        '''self.setMaintenanceSwitch()
         self.updateBooleanAuth() #Uncomment when hardware is not connected
         self.preventCollision() #Function to prevent occupancy collisions
-        self.sendUpdatedBlocks.emit(self.allBlocks) #Uncomment when hardware is not connected
+        self.sendUpdatedBlocks.emit(self.allBlocks) #Uncomment when hardware is not connected'''
     
     def selectBlock(self):
         self.frameLight.setEnabled(False)
@@ -488,6 +496,8 @@ class TrackController_HW(QMainWindow):
                     if self.allBlocks[index+2] not in tempSkip:
                         self.allBlocks[index+2].authority = True
             if block.blockSection in biDirection:
+                if block.ID == 'D13':
+                    continue
                 if self.allBlocks[index-1] in self.previousOccupiedBlock:
                     if block.occupied == True:
                         self.allBlocks[index-1].authority = False

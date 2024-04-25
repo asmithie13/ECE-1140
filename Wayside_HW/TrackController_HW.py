8349250251eefc6d1c46dcf750453825799f6099
20lillianjones <lej40@pitt.edu>
2024-04-25 00:51:29 -0400

    uncommented hardware for testing

--- Wayside_HW/TrackController_HW.py
+++ Wayside_HW/TrackController_HW.py
@@ -13,7 +13,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
 #Enable serial communication:
-#serialObject = serial.Serial('COM8', 9600)
+serialObject = serial.Serial('COM8', 9600)
 
 from Wayside_HW.TrackController_HW_TB import *
 from Wayside_HW.readTrackFile import *
@@ -181,7 +181,7 @@ class TrackController_HW(QMainWindow):
         occupiedBlockBytes = occupiedBlockString.encode()
 
         '''BEGIN SERIAL COMMUNICATION'''
-        '''serialObject.write(occupiedBlockBytes)
+        serialObject.write(occupiedBlockBytes)
         #Receiving serial responses from the Raspberry Pi:
         copyBlocks = self.allBlocks
         attributeList = []
@@ -203,7 +203,7 @@ class TrackController_HW(QMainWindow):
                     else:
                         attributeList.append(myAttribute)
                 if breakFlag == 1:
-                    break'''
+                    break
         
         #Parse PLC file and adjust blocks accordingly:
         self.allBlocks = newParse(occupiedBlockSections, self.allBlocks)
@@ -216,7 +216,7 @@ class TrackController_HW(QMainWindow):
             elif block.CROSSING == True:
                 attributeListSoftware.append(str(block.crossingState))
 
-        '''if attributeList != attributeListSoftware:
+        if attributeList != attributeListSoftware:
             self.lineEditHardware.setText("ERRORS DETECTED. STOPPING ALL TRAINS.")
             for block in self.allBlocks:
                 block.authority = False
@@ -226,9 +226,9 @@ class TrackController_HW(QMainWindow):
             self.setMaintenanceSwitch()
             self.updateBooleanAuth()
             self.preventCollision()
-            self.sendUpdatedBlocks.emit(self.allBlocks)'''
+            self.sendUpdatedBlocks.emit(self.allBlocks)
         
-        self.setMaintenanceSwitch()
+        '''self.setMaintenanceSwitch()
         self.updateBooleanAuth() #Uncomment when hardware is not connected
         self.preventCollision() #Function to prevent occupancy collisions
         self.sendUpdatedBlocks.emit(self.allBlocks) #Uncomment when hardware is not connected'''
@@ -567,7 +567,7 @@ class TrackController_HW(QMainWindow):
             if block.blockSection in biDirection:
                 if block.ID == 'D13':
                     continue
-                if block.ID == 'F22' and self.biFlag >= 2:
+                if block.ID == 'F22' and self.biFlag >= 2: #Should change
                     if block.occupied == True:
                         self.allBlocks[index-1].authority = False
                         self.allBlocks[index-2].authority = False

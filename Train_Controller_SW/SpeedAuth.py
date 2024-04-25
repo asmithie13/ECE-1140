0db2807e7daca7bc884dd3b5fc915c33b58b4b83
asmithie13 <ajs392@pitt.edu>
2024-04-25 12:58:50 -0400

    Adding more red line funcitonality

--- Train_Controller_SW/SpeedAuth.py
+++ Train_Controller_SW/SpeedAuth.py
@@ -21,6 +21,7 @@ class Vital_Speed_Auth():
         self.stop_at_station_sig = stop_at_station_sig
         self.NonVital = NonVital
         self.bool_auth_enabled = True
+        self.stopFlag = False
 
     def Control_Current_Speed(self,newSpeed):
         self.ui.lcdCurSpd.display("{:.2f}".format(newSpeed))
@@ -136,7 +137,8 @@ class Vital_Speed_Auth():
             elif self.ui.buttonMan.isChecked() :
                 self.ui.vertSliderPow.setEnabled(True)
 
-        elif self.decimal_m_auth < 1 and self.ui.lcdCurSpd == 0:
+        elif self.ui.lcdAuth.value() <= 0 and self.ui.lcdCurSpd.value == 0.0 and not self.stopFlag:
+            self.stopFlag = True
             self.stop_at_station_sig.emit(1)
     
         if self.ui.buttonAuto.isChecked():
@@ -159,7 +161,7 @@ class Vital_Speed_Auth():
                 self.ui.buttonDoorR.toggle()
 
     def Control_Authority(self,auth):
-
+        self.stopFlag = False
         if self.decimal_m_auth < 0:
             self.decimal_m_auth = self.decimal_m_auth + auth
         else :   
